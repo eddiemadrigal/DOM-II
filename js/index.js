21abf5ae72d4724bb517868af9f1531452e39fd7
Eddie Madrigal <edmadrigal@yahoo.com>
2020-02-11 12:30:15 -0800

    mouseover / mouseout event complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// Your code goes here
+/*
+* [X] `mouseover`
+* [ ] `keydown`
+* [ ] `wheel`
+* [ ] `drag / drop`
+* [ ] `load`
+* [ ] `focus`
+* [ ] `resize`
+* [ ] `scroll`
+* [ ] `select`
+* [ ] `dblclick`
+*/
+
+const h1Tag = document.querySelector('h1.logo-heading');
+h1Tag.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    h1Tag.style.cursor = 'pointer';
+    h1Tag.style.transform = 'scale(1.2)';
+});
+h1Tag.addEventListener('mouseout', (e) => {
+    e.stopPropagation();
+    h1Tag.style.transform = 'scale(1.0)';
+});
+
+
